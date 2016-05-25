71bbcb8b19e159ae485b0d45136cf506e3ed1d72
uichunter <pankettingpanda@gmail.com>
2016-05-25 15:38:24 -0500

    Import partical package.
    Add day cycle to simulate the sunrise and sunset.
    
    Add player sound to control the inner voice of player.

---
+++ Assets/Script/Light/DayCycle.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof (Light))]
+public class DayCycle : MonoBehaviour {
+	public Transform m_SunTransform;
+	public float m_TimeScale;
+	public float m_DefaultTime;
+	public bool m_isSunStartCycle;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (m_isSunStartCycle){
+			float anglePerFrame = Time.deltaTime / 360 * m_TimeScale;
+			transform.RotateAround(transform.position,Vector3.forward,anglePerFrame);
+		}
+	}
+}
---
+++ Assets/Script/Player/PlayerSound.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSound : MonoBehaviour {
+public AudioSource m_PlyerAudioSource;
+public AudioClip[] m_PlayerLineClips;
+public float m_PlayerLines01Time;
+public bool[] m_IsLinesPlayed;
+
+	// Use this for initialization
+	void Start () {
+		for (int i =0;i < m_IsLinesPlayed.Length; i++){// Reset the m_IsLinePlayed list;
+			m_IsLinesPlayed[i] = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Talk();
+	}
+
+	void Talk ()
+	{
+		if (Time.timeSinceLevelLoad > m_PlayerLines01Time && !m_IsLinesPlayed[0]){
+			m_PlyerAudioSource.clip = m_PlayerLineClips[0];
+			m_PlyerAudioSource.Play();
+			m_IsLinesPlayed[0] = true;
+			print("What the hell is going here.'");
+		}
+	}
+}
